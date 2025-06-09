3dd1ec48676a8954201eb3e041dcce5395d5f218
Nhat Minh Phan <nhatminh.phan.2k4@gmail.com>
2025-06-09 22:56:41 +0700

    Add distinct_values() to Plan interface for optimization

--- MudopDB_v1/include/plan/plan.hpp
+++ MudopDB_v1/include/plan/plan.hpp
@@ -10,6 +10,11 @@ namespace record {
 class Schema;
 }
 
+/**
+ * Abstract base class for all plan types.
+ *
+ * Corresponds to PlanControl trait in Rust (NMDB2/src/plan/plan.rs)
+ */
 class Plan {
 public:
     virtual ~Plan() = default;
@@ -17,6 +22,7 @@ public:
     virtual std::unique_ptr<Scan> open() = 0;
     virtual size_t blocks_accessed() const = 0;
     virtual size_t records_output() const = 0;
+    virtual size_t distinct_values(const std::string& fldname) const = 0;
     virtual std::shared_ptr<record::Schema> schema() const = 0;
 };
 
