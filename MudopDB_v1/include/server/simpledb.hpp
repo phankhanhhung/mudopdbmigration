c313f1350828e657562cb8d2ddcbf4b305a61937
Nhat Minh Phan <nhatminh.phan.2k4@gmail.com>
2025-06-27 22:41:33 +0700

    Add Transaction and Metadata support to SimpleDB

--- MudopDB_v1/include/server/simpledb.hpp
+++ MudopDB_v1/include/server/simpledb.hpp
@@ -4,6 +4,8 @@
 #include "file/filemgr.hpp"
 #include "log/logmgr.hpp"
 #include "buffer/buffermgr.hpp"
+#include "metadata/metadatamgr.hpp"
+#include "tx/transaction.hpp"
 #include <memory>
 #include <string>
 
@@ -12,17 +14,23 @@ namespace server {
 class SimpleDB {
 public:
     SimpleDB(const std::string& dirname, size_t blocksize, size_t buffsize);
+    SimpleDB(const std::string& dirname);
 
+    std::shared_ptr<tx::Transaction> new_tx();
+    std::shared_ptr<metadata::MetadataMgr> md_mgr() const;
     std::shared_ptr<file::FileMgr> file_mgr() const;
     std::shared_ptr<log::LogMgr> log_mgr() const;
     std::shared_ptr<buffer::BufferMgr> buffer_mgr() const;
 
 private:
+    static constexpr size_t BLOCK_SIZE = 400;
+    static constexpr size_t BUFFER_SIZE = 8;
     static constexpr const char* LOG_FILE = "simpledb.log";
 
     std::shared_ptr<file::FileMgr> fm_;
     std::shared_ptr<log::LogMgr> lm_;
     std::shared_ptr<buffer::BufferMgr> bm_;
+    std::shared_ptr<metadata::MetadataMgr> mdm_;
 };
 
 } // namespace server
--- MudopDB_v1/src/server/simpledb.cpp
+++ MudopDB_v1/src/server/simpledb.cpp
@@ -1,4 +1,5 @@
 #include "server/simpledb.hpp"
+#include <iostream>
 
 namespace server {
 
@@ -8,6 +9,30 @@ SimpleDB::SimpleDB(const std::string& dirname, size_t blocksize, size_t buffsize
     bm_ = std::make_shared<buffer::BufferMgr>(fm_, lm_, buffsize);
 }
 
+SimpleDB::SimpleDB(const std::string& dirname) {
+    fm_ = std::make_shared<file::FileMgr>(dirname, BLOCK_SIZE);
+    lm_ = std::make_shared<log::LogMgr>(fm_, LOG_FILE);
+    bm_ = std::make_shared<buffer::BufferMgr>(fm_, lm_, BUFFER_SIZE);
+
+    auto tx = new_tx();
+    bool isnew = fm_->is_new();
+
+    if (isnew) {
+        std::cout << "creating new database" << std::endl;
+    } else {
+        std::cout << "recovering existing database" << std::endl;
+        tx->recover();
+    }
+
+    mdm_ = std::make_shared<metadata::MetadataMgr>(isnew, tx);
+    tx->commit();
+}
+
+std::shared_ptr<tx::Transaction> SimpleDB::new_tx() {
+    return std::make_shared<tx::Transaction>(fm_, lm_, bm_);
+}
+
+std::shared_ptr<metadata::MetadataMgr> SimpleDB::md_mgr() const { return mdm_; }
 std::shared_ptr<file::FileMgr> SimpleDB::file_mgr() const { return fm_; }
 std::shared_ptr<log::LogMgr> SimpleDB::log_mgr() const { return lm_; }
 std::shared_ptr<buffer::BufferMgr> SimpleDB::buffer_mgr() const { return bm_; }
